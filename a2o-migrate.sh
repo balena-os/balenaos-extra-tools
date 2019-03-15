5ea877245a3d93b2f9e9332c609687071b4f6bad
Andrei Gherzan <andrei@balena.io>
2019-03-15 16:36:30 +0000

    a2o-migrate.sh: Fix
    Signed-off-by: Andrei Gherzan <andrei@balena.io>

--- a2o-migrate.sh
+++ a2o-migrate.sh
@@ -23,15 +23,16 @@ log () {
 
 clean_containers () {
 	# Make sure no new containers come up or are running
+	log "Stopping systemd services ..."
 	systemctl stop update-resin-supervisor.timer
 	systemctl stop update-resin-supervisor
 	systemctl stop resin-supervisor
 	systemctl start balena
 	if [ -n "$("$ENGINE" ps -a -q)" ]; then
 		log "Found containers stopping ..."
-		"$ENGINE" stop "$("$ENGINE" ps -a -q)"
+		"$ENGINE" stop "$("$ENGINE" ps -a -q)" &> /dev/null
 		log "Found containers removing ..."
-		"$ENGINE" rm "$("$ENGINE" ps -a -q)"
+		"$ENGINE" rm "$("$ENGINE" ps -a -q)" &> /dev/null
 		log "Done."
 	fi
 	systemctl stop balena
@@ -46,25 +47,31 @@ fi
 
 log "Using $DOCKERDIR as $ENGINE root directory."
 
+# TODO: handle containers as well
 clean_containers
 
 mkdir -p "$DOCKERDIR/overlay2/l"
-log "Migrating layers and generating layer IDs ..."
+log "Migrating layers ..."
 for aufs_layerpath in "$DOCKERDIR"/aufs/diff/*; do
 	layer="$(basename "$aufs_layerpath")"
-	log "	$layer ..."
+	log "---> $layer ..."
 
+	# root layer directrory
 	mkdir -p "$OVERLAYDIR/$layer"
 
-	# link	
-	generateID > "$OVERLAYDIR/$layer/link"
-	ln -s "../$layer" "$OVERLAYDIR/l/$(cat "$OVERLAYDIR/$layer/link")"
+	# link
+	if [ ! -f "$OVERLAYDIR/$layer/link" ]; then
+		printf "%s" "$(generateID)" > "$OVERLAYDIR/$layer/link"
+		ln -s "../$layer/diff" "$OVERLAYDIR/l/$(cat "$OVERLAYDIR/$layer/link")"
+	fi
 
 	# lower
+	lower=""
 	while IFS= read -r parent_layer; do
 		if [ ! -f "$OVERLAYDIR/$parent_layer/link" ]; then
 			mkdir -p "$OVERLAYDIR/$parent_layer"
-			generateID > "$OVERLAYDIR/$parent_layer/link"
+			printf "%s" "$(generateID)" > "$OVERLAYDIR/$parent_layer/link"
+			ln -s "../$parent_layer/diff" "$OVERLAYDIR/l/$(cat "$OVERLAYDIR/$parent_layer/link")"
 		fi
 		parent_layer_id="$(cat "$OVERLAYDIR/$parent_layer/link")"
 		if [ -z "$lower" ]; then
@@ -75,16 +82,15 @@ for aufs_layerpath in "$DOCKERDIR"/aufs/diff/*; do
 	done < "$AUFSDIR/layers/$layer"
 	if [ -n "$lower" ]; then
 		echo -n "$lower" > "$OVERLAYDIR/$layer/lower"
+		
+		# work
+		mkdir -p "$OVERLAYDIR/$layer/work"
 	fi
 
-	# work
-	mkdir -p "$OVERLAYDIR/$layer/work"
 
 	# diff
 	mkdir -p "$OVERLAYDIR/$layer"
 	mv "$aufs_layerpath" "$OVERLAYDIR/$layer/diff"
-
-	sync
 done
 
 # image
@@ -93,13 +99,20 @@ mv "$DOCKERDIR/image/aufs" "$DOCKERDIR/image/overlay2"
 # Clean
 rm -rf "$AUFSDIR"
 
-log "Switching balena engine to overlay2... "
+log "Switching balena engine to overlay2 ... "
 mount -o remount,rw /
 sed -i "s/aufs/overlay2/g" /lib/systemd/system/balena.service
 if [ -f "/etc/systemd/system/balena.service.d/balena.conf" ]; then
 	sed -i "s/aufs/overlay2/g" /etc/systemd/system/balena.service.d/balena.conf
 fi
 systemctl daemon-reload
-
-log "Enjoy overlay2!"
 mount -o remount,ro /
+
+sync
+
+log "Bringing back systemd services ..."
+systemctl start balena
+systemctl start resin-supervisor
+systemctl start update-resin-supervisor.timer
+
+log "Done!"
